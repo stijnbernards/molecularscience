16c33b68622d2207f93d70e6878dccebf0313c8c
stijnhero <stijn@bernards.nl>
2014-03-10 22:11:05 +0100

    Improved heat conducting system

--- moditems/BlockHeatConductant.java
+++ moditems/BlockHeatConductant.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.ColorizerGrass;
@@ -16,8 +17,6 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockHeatConductant extends BlockContainer{
-	
-	int color = 0x000000;
 	
 	public BlockHeatConductant(Material material) {
 		super(material);
@@ -30,24 +29,30 @@ public class BlockHeatConductant extends BlockContainer{
     {
         return 0x000000;
     }
-
+    
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileEntityHeatConductant(this);
 	}
 	
-    public void updateTick(World p_149674_1_, int p_149674_2_, int p_149674_3_, int p_149674_4_, Random p_149674_5_)
+    public int updateTick(World p_149674_1_, int p_149674_2_, int p_149674_3_, int p_149674_4_, Random p_149674_5_)
     {
+    	int temp = 0;
     	TileEntityHeatConductant tile = (TileEntityHeatConductant) p_149674_1_.getTileEntity(p_149674_2_, p_149674_3_, p_149674_4_);
     	if (tile != null)
     	{
-    		color = tile.color;
+    		temp = (int) tile.Temperature;
     	}
+    	return temp;
     }
 	
     @SideOnly(Side.CLIENT)
     public int colorMultiplier(IBlockAccess p_149720_1_, int p_149720_2_, int p_149720_3_, int p_149720_4_)
     {
-        return color;
+   		int rr = 0;
+   		int gg = 0;
+   		int bb = 0;
+   		rr = (int) ((16711680 / 700) * this.temp);
+   		return (rr / 9 & 255) << 16 | (gg / 9 & 255) << 8 | bb / 9 & 255;
     }
 }
--- moditems/TileEntityHeatConductant.java
+++ moditems/TileEntityHeatConductant.java
@@ -1,5 +1,7 @@
 package molecularscience.moditems;
 
+import org.apache.logging.log4j.core.pattern.ConverterKeys;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import molecularscience.MolecularScience;
@@ -12,7 +14,7 @@ import net.minecraft.world.World;
 
 public class TileEntityHeatConductant extends TileEntity{
 
-	double Temperature = 0;
+	double Temperature = 21;
 	
 	int color = 0x000000;
 	
@@ -27,6 +29,7 @@ public class TileEntityHeatConductant extends TileEntity{
    	{
    		super.writeToNBT(nbt);
       	nbt.setDouble("Temperature", Temperature);
+      	nbt.setInteger("Color", color);
    	}
 
    	@Override
@@ -34,6 +37,7 @@ public class TileEntityHeatConductant extends TileEntity{
    	{
    		super.readFromNBT(nbt);
       	this.Temperature = nbt.getDouble("Temperature");
+      	this.color = nbt.getInteger("Color");
    	}
    	
    	@Override
@@ -43,23 +47,50 @@ public class TileEntityHeatConductant extends TileEntity{
    	
    	@Override
    	public void updateEntity() {
-   		checkblocks(worldObj.getBlock(xCoord+1, yCoord, zCoord).getLocalizedName());
-   		worldObj.scheduleBlockUpdate(xCoord, yCoord, zCoord, Block, 50);
+   		checkblocks(worldObj.getBlock(xCoord+1, yCoord, zCoord).getLocalizedName(), "1 0 0");
+   		checkblocks(worldObj.getBlock(xCoord-1, yCoord, zCoord).getLocalizedName(), "-1 0 0");
+   		checkblocks(worldObj.getBlock(xCoord, yCoord+1, zCoord).getLocalizedName(), "0 1 0");
+   		checkblocks(worldObj.getBlock(xCoord, yCoord-1, zCoord).getLocalizedName(), "0 -1 0");
+   		checkblocks(worldObj.getBlock(xCoord, yCoord, zCoord+1).getLocalizedName(), "0 0 1");
+   		checkblocks(worldObj.getBlock(xCoord, yCoord, zCoord-1).getLocalizedName(), "0 0 -1");
+   		worldObj.scheduleBlockUpdate(xCoord, yCoord, zCoord, Block, 1);
    		this.worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
    	}
    	
-   	public void checkblocks(String block){
-   		if(block == "Fire"){
-   			if(Temperature != 600){
+   	public void checkblocks(String block, String coord){
+   		if(block.equals("Fire")){
+   			if(Temperature <= 600.0){
    				Temperature = Temperature + 0.1;
    			}
    		}
-   		if(block == "Lava"){
-   			if(Temperature != 700){
+   		if(block.equals("Lava")){
+   			if(Temperature <= 700.0){
    				Temperature = Temperature + 0.5;
    			}else{
-   				System.out.println("BOOM");
+   				
    			}
+   		}
+   		if(block.equals("Water")){
+   			if(Temperature >= 10.0){
+   				Temperature = Temperature - 0.3;
+   			}
+   		}
+   		if(block.equals("HeatConductant")){
+   			int gettiletemp = 0;
+   			
+   			String[] coords = coord.split(" ");
+   			
+   	    	TileEntityHeatConductant tile = (TileEntityHeatConductant) worldObj.getTileEntity(xCoord + Integer.parseInt(coords[0]), yCoord + Integer.parseInt(coords[1]), zCoord + Integer.parseInt(coords[2]));
+   	    	if (tile != null)
+   	    	{
+   	    		gettiletemp = (int) tile.Temperature;
+   	    	}
+   	    	
+   	    	if(gettiletemp >= this.Temperature){
+   	    		double add = gettiletemp - this.Temperature;
+   	    		add = add / 1000;
+   	    		Temperature = Temperature + add;
+   	    	}
    		}
    	}
 }
